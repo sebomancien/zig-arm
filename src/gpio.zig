12b11b32ef5e59d79507f7ac78c8bf18c3bef100
sebomancien <sebomancien@hotmail.com>
2026-05-18 21:28:05 -0400

    Switched to GPIO struct implementation

--- src/gpio.zig
+++ src/gpio.zig
@@ -1,58 +1,68 @@
 const reg = @import("stm32f401xe.zig");
 
-pub const Port = extern struct {
-    MODER: u32, // GPIO port mode register,               Address offset: 0x00
-    OTYPER: u32, // GPIO port output type register,        Address offset: 0x04
-    OSPEEDR: u32, // GPIO port output speed register,       Address offset: 0x08
-    PUPDR: u32, // GPIO port pull-up/pull-down register,  Address offset: 0x0C
-    IDR: u32, // GPIO port input data register,         Address offset: 0x10
-    ODR: u32, // GPIO port output data register,        Address offset: 0x14
-    BSRR: u32, // GPIO port bit set/reset register,      Address offset: 0x18
-    LCKR: u32, // GPIO port configuration lock register, Address offset: 0x1C
-    AFR: [2]u32, // GPIO alternate function registers,     Address offset: 0x20-0x24
+const Register = extern struct {
+    MODER: u32,
+    OTYPER: u32,
+    OSPEEDR: u32,
+    PUPDR: u32,
+    IDR: u32,
+    ODR: u32,
+    BSRR: u32,
+    LCKR: u32,
+    AFR: [2]u32,
 };
 
-pub const Pin = u4;
+pub const Gpio = struct {
+    pub const Mode = enum(u2) {
+        input = 0b00,
+        output = 0b01,
+        alternateFunction = 0b10,
+        analog = 0b11,
+    };
 
-pub const PORTA: *volatile Port = @ptrFromInt(reg.GPIOA_BASE);
-pub const PORTB: *volatile Port = @ptrFromInt(reg.GPIOB_BASE);
-pub const PORTC: *volatile Port = @ptrFromInt(reg.GPIOC_BASE);
-pub const PORTD: *volatile Port = @ptrFromInt(reg.GPIOD_BASE);
-pub const PORTE: *volatile Port = @ptrFromInt(reg.GPIOE_BASE);
-pub const PORTH: *volatile Port = @ptrFromInt(reg.GPIOH_BASE);
+    pub const Port = enum { a, b, c, d, e, h };
 
-pub const Mode = enum(u2) {
-    Input = 0b00,
-    Output = 0b01,
-    AlternateFunction = 0b10,
-    Analog = 0b11,
-};
+    pub const Pin = u4;
 
-pub fn SetMode(port: *volatile Port, pin: Pin, mode: Mode) void {
-    const shift: u5 = @as(u5, pin) << 1;
-    port.MODER = (port.MODER & ~(@as(u32, 0b11) << shift)) | (@as(u32, @intFromEnum(mode)) << shift);
-}
+    port: *volatile Register,
+    pin: Pin,
 
-pub fn Read(port: *volatile Port, pin: Pin) bool {
-    return (port.IDR & ((@as(u32, 1) << pin))) != 0;
-}
-
-pub fn Set(port: *volatile Port, pin: Pin) void {
-    port.BSRR = @as(u32, 1) << pin;
-}
-
-pub fn Clear(port: *volatile Port, pin: Pin) void {
-    port.BSRR = @as(u32, 1) << (@as(u5, pin) + 16);
-}
-
-pub fn Toggle(port: *volatile Port, pin: Pin) void {
-    port.ODR ^= @as(u32, 1) << pin;
-}
-
-pub fn Write(port: *volatile Port, pin: Pin, value: bool) void {
-    if (value) {
-        Set(port, pin);
-    } else {
-        Clear(port, pin);
+    pub fn init(port: Port, pin: Pin) Gpio {
+        return .{
+            .port = switch (port) {
+                .a => @ptrFromInt(reg.GPIOA_BASE),
+                .b => @ptrFromInt(reg.GPIOB_BASE),
+                .c => @ptrFromInt(reg.GPIOC_BASE),
+                .d => @ptrFromInt(reg.GPIOD_BASE),
+                .e => @ptrFromInt(reg.GPIOE_BASE),
+                .h => @ptrFromInt(reg.GPIOH_BASE),
+            },
+            .pin = pin,
+        };
     }
-}
+
+    pub fn setMode(self: Gpio, mode: Mode) void {
+        const shift: u5 = @as(u5, self.pin) << 1;
+        self.port.MODER = (self.port.MODER & ~(@as(u32, 0b11) << shift)) | (@as(u32, @intFromEnum(mode)) << shift);
+    }
+
+    pub fn read(self: Gpio) bool {
+        return (self.port.IDR & (@as(u32, 1) << self.pin)) != 0;
+    }
+
+    pub fn set(self: Gpio) void {
+        self.port.BSRR = @as(u32, 1) << self.pin;
+    }
+
+    pub fn clear(self: Gpio) void {
+        self.port.BSRR = @as(u32, 1) << (@as(u5, self.pin) + 16);
+    }
+
+    pub fn toggle(self: Gpio) void {
+        self.port.ODR ^= @as(u32, 1) << self.pin;
+    }
+
+    pub fn write(self: Gpio, value: bool) void {
+        if (value) self.set() else self.clear();
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,14 @@
 const RCC = @import("rcc.zig");
 const SysTick = @import("systick.zig");
-const GPIO = @import("gpio.zig");
+const Gpio = @import("gpio.zig").Gpio;
 
-const LED_PORT = GPIO.PORTA;
-const LED_PIN: GPIO.Pin = 5;
+const led = Gpio.init(.a, 5);
 
 pub export fn main() void {
     RCC.EnableGPIOA();
 
-    GPIO.SetMode(LED_PORT, LED_PIN, .Output);
-    GPIO.Clear(LED_PORT, LED_PIN);
+    led.setMode(.output);
+    led.clear();
 
     SysTick.Init(800000);
 
@@ -17,5 +16,5 @@ pub export fn main() void {
 }
 
 pub export fn SysTick_Handler() void {
-    GPIO.Toggle(LED_PORT, LED_PIN);
+    led.toggle();
 }
